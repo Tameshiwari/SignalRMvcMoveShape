7845ad09213ba90ad1acdda47f6fb4057d513ce4
Tameshiwari <denyschamberland@videotron.ca>
2013-12-19 13:38:40 -0500

    SignalR with [x,y] item position Broadcast
    This sample shows how SignalR hub can be used to transfer real time data
    infos.

---
+++ SignalRMvcMoveShape/SignalRMvcMoveShape/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRMvcMoveShape
+{
+    //[assembly: OwinStartup(typeof(MoveShapeDemo.Startup))]
+    [assembly: OwinStartup(typeof(SignalRMvcMoveShape.Startup))]
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
